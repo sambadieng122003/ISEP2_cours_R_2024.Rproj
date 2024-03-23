8a33765bb8c5996f9bff5134558a5dd2fb9f958d
sambadieng122003 <160957672+sambadieng122003@users.noreply.github.com>
2024-03-23 08:00:02 +0000

    Update exos_du_23_03_Samba_Dieng.R

--- exos_du_23_03_Samba_Dieng.R
+++ exos_du_23_03_Samba_Dieng.R
@@ -14,23 +14,25 @@ fusion$depenses_cons_menages <- fusion$Qtty_cons*fusion$prix_unitaire
 
 
 #Valeurs abberantes:: détection et corections
-visualisation <- boxplot(fusion$Qtty_cons, 
-                         main = "Boxplot quantite consommé", # Titre du graphique
-                         ylab = "Dépenses de consommation", # Nom de l'axe des ordonnées
+# DECTECTION
+fusion$quantite_cons_kg <- fusion$Qtty_cons * fusion$poids/1000
+visualisation <- boxplot(fusion$quantite_cons_kg, 
+                         main = " quantite consommé", # Titre du graphique
+                         ylab = "Qtty_cons", # Nom de l'axe des ordonnées
                          col = "skyblue", 
                          border = "blue", 
                          horizontal = TRUE)
-Q1 <- quantile(fusion$qtty_cons_kg, 0.25)
-Q3 <- quantile(fusion$qtty_cons_kg, 0.75)
+Q1 <- quantile(fusion$Qtty_cons_kg, 0.25)
+Q3 <- quantile(fusion$Qtty_cons_kg, 0.75)
 EQR <- Q3 - Q1
 
 sup <- Q3 + 1.5 * EQR
 inf <- Q1 - 1.5 * EQR
-fusion$quantite_cons_kg <- fusion$Qtty_cons * fusion$poids/1000
+
 view(fusion)
 valeurs_aberrantes_inf <- fusion$fusion$quantite_cons_kg < inf
 valeurs_aberrantes_sup <- fusion$fusion$quantite_cons_kg > sup
-
+# TRAITEMENT
 # Remplacer les valeurs aberrantes par la moyenne des données non aberrantes
 moyenne_sans_aberrantes <- mean(fusion$quantite_cons_kg[!valeurs_aberrantes_inf & !valeurs_aberrantes_sup])
 fusion$fusion$quantite_cons_kg[valeurs_aberrantes_inf | valeurs_aberrantes_sup] <- moyenne_sans_aberrantes
